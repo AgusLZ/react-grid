1ce2d895225cd92602d1380a57563d4f67320717
AgusLZ <87140104+AgusLZ@users.noreply.github.com>
2021-10-04 13:34:28 -0300

    Add files via upload
    gfhfgkji

---
+++ Main.jsx
@@ -0,0 +1,20 @@
+import "./styles/Main.css";
+import Imagen from "./Imagen"
+import"./main.css"
+const Main = () => {
+  return <main>
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+        <Imagen />
+  </main>;
+};
+
+export default Main;
